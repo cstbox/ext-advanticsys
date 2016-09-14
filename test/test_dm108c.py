36941a5849f5cfa0f86352386ac297a8fa2e23fe
Eric Pascual <eric.pascual@cstb.fr>
2016-09-14 11:21:03 +0200

    added

---
+++ test/test_dm108c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from pycstbox.advanticsys.dm108c import DM108CInstrument
+
+
+class DM108CTestCase(unittest.TestCase):
+    def test_01(self):
+        reg = DM108CInstrument.PULSE_CNT
+
+        self.assertEquals(reg.decode(''.join((chr(i) for i in xrange(1, 7)))), 605040302.01)
+
+
+if __name__ == '__main__':
+    unittest.main()
