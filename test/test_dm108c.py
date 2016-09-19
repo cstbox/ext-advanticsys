e6405abd2b0f6c0994382e62eadb0b3cfb7946d3
Eric Pascual <eric.pascual@cstb.fr>
2016-09-19 12:28:26 +0200

    added new test

--- test/test_dm108c.py
+++ test/test_dm108c.py
@@ -12,6 +12,11 @@ class DM108CTestCase(unittest.TestCase):
 
         self.assertEquals(reg.decode(''.join((chr(i) for i in xrange(1, 7)))), 605040302.01)
 
+    def test_02(self):
+        reg = DM108CInstrument.PULSE_CNT
+
+        self.assertEquals(reg.decode(''.join((chr(i) for i in [0] * 6))), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
